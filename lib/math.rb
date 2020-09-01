bd78c6a90ec7ef7a3aa89e7ec4b284e134dc805f
SunJet Liu <sunjetliu@gmail.com>
2020-09-01 00:54:24 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  diff
 end
 
 def division(num1, num2)
-
+  div = num1/num2
+  div
 end
 
 def multiplication(num1, num2)
-
+  prod = num1*num2
+  prod
 end
 
 def modulo(num1, num2)
-
+  mod = num1%num2
+  mod
 end
 
 def square_root(num)
-
+  rt = Math.sqrt(num)
+  rt
 end
